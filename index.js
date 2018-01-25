81bd4f96e0aeb769893b975ed7c9aefdd0bd857a
giftkugel <github@giftkugel.de>
2018-01-25 11:50:24 +0100

    Improved response+logging

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const HTTP_PORT = 8080;
 const UNIQUE_ID = CRYPTO.randomBytes(16).toString("hex");
 
 HTTP.createServer(function (req, res) {
-    console.log('Incoimg request:', req.method, req.url);
-    res.write('Hello World with id ' + UNIQUE_ID + ' at ' + req.url + '\n');
+    const CURRENT_DATE = new Date();
+    console.log(CURRENT_DATE.toISOString(), '-', 'Incoimg request:', req.method, req.url);
+    res.write('[' + CURRENT_DATE.toISOString()+ '] Hello World with id ' + UNIQUE_ID + ' at ' + req.url + '\n');
     res.end();
+}).on('error', (error) => {
+    console.error(error);
+}).on('listening', () => {
+    console.log('HTTP Server is listening now on port', HTTP_PORT);
 }).listen(HTTP_PORT);
 
-console.log('HTTP Server is listening on port', HTTP_PORT);
