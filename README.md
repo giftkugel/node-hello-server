7c7f2bb3d446b4846176fa679ad4e8c9d2468cc6
giftkugel <github@giftkugel.de>
2020-10-21 16:03:20 +0200

    Add headers, improve reponse and logging

--- README.md
+++ README.md
@@ -3,36 +3,69 @@ A simple stateless HTTP server written in Node.js which returns a timestamp, 'He
 This server can be used to check loadbalancing as every instance returns an own unique string.
 
 ## Requirements
-* [Git](https://git-scm.com/) and [Node.js](https://nodejs.org/en/) or [Docker](https://www.docker.com/)
+* [Git](https://git-scm.com/) and [Node.js](https://nodejs.org/en/) or [Docker](https://www.docker.com/) / [Kubernetes](https://kubernetes.io/)
 
 ## Source version
+
+Clone
 ```
 git clone https://github.com/giftkugel/node-hello-server.git
 cd node-hello-server
 ```
 
+Run the server
 ```
 node index.js
 ```
 
 ## Docker image from [Docker Hub](https://hub.docker.com/r/giftkugel/node-hello/)
 
-Pull the image
-```
-docker pull giftkugel/node-hello:latest
-```
-
 Start the container
 ```
-docker run -d -p 8080:8080 giftkugel/node-hello:latest
+docker run --name node-hello -d -p 8080:8080 giftkugel/node-hello:latest
+```
+
+## Running on [Kubernetes](https://kubernetes.io/) (e.g. bundled with [Docker Desktop](https://www.docker.com/products/docker-desktop))
+
+Create deployment
+```
+kubectl create deployment hello --image=giftkugel/node-hello:latest
+```
+
+Expose the deployment
+```
+kubectl expose deployment hello --port=8080 --target-port=8080 --name=hello-service --type=LoadBalancer
 ```
 
 ## Usage
+
+Default
 ```
 curl http://localhost:8080
 ```
 
-### Example response
+### Response
+
+#### Plain text
 ```
-[2020-10-19T15:27:18.770Z] Hello World: id=3a5a8f84, path=/test, node=v12.16.1, count=7
+2020-10-21T13:52:47.792Z] - Hello World          - request=5bf3b6dec71250b9, id=142a4a66, ip=::1, method=GET, node=v12.9.0, count=0000000003, path=/
+```
+
+#### Custom headers in response
+```
+Server: node-hello-server 142a4a66
+Request-Id: 5bf3b6dec71250b9
+Node-Version: v12.9.0
+Count: 0000000003
+Connection-Date: 2020-10-21T13:52:47.792Z
+```
+
+### Configuration
+
+#### Wait for a reponse
+You can set the `wait` query parameter to let the server wait an amount of time before the answer is send
+
+Example: Wait 5 seconds
+```
+curl http://localhost:8080?wait=5000
 ```
--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const HTTP = require('http');
-const CRYPTO = require("crypto");
+const http = require('http');
+const crypto = require("crypto");
 
-const HTTP_PORT = 8080;
-const UNIQUE_ID = CRYPTO.randomBytes(4).toString("hex");
+const httpPort = 8080;
+const uniqueId = crypto.randomBytes(4).toString("hex");
 
 let count = 0;
 
-HTTP.createServer((req, res) => {
-    const CURRENT_DATE = new Date();
-    count++;
-    console.log(CURRENT_DATE.toISOString(), '-', 'Incoimg request:', req.method, req.url);
-    res.write('[' + CURRENT_DATE.toISOString()+ '] Hello World: id=' + UNIQUE_ID + ', path=' + req.url + ', node=' + process.version + ', count=' + count + '\n');
+const createOutput = (text, req, currentDate, currentCount, requestId) => {
+    return `${currentDate} - ${text.padEnd(20)} - request=${requestId}, id=${uniqueId}, ip=${req.connection.remoteAddress}, method=${req.method}, node=${process.version}, count=${currentCount}, path=${req.url}`;
+}
+
+const writeResponse = (req, res, currentDate, currentDateString, currentCount, requestId) => {
+    const responseOutput = createOutput('Hello World', req, currentDate, currentCount, requestId) + "\n";
+    const logOutput = createOutput('Response sent', req, currentDate, currentCount, requestId);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Server', `node-hello-server ${uniqueId}`);
+    res.setHeader('Request-Id', requestId);
+    res.setHeader('Node-Version', process.version);
+    res.setHeader('Count', currentCount);
+    res.setHeader('Connection-Date', currentDateString);
+    res.write(responseOutput);
     res.end();
-}).on('error', (error) => {
+    console.log(logOutput);
+}
+
+const handleRequest = (req, res) => {
+    const requestId = crypto.randomBytes(8).toString("hex");
+    const currentCount = `${count++}`.padStart(10, '0');
+    const currentDateString = new Date().toISOString();
+    const currentDate = `[${currentDateString}]`;
+    const requestUrl = new URL(req.url, `http://${req.headers.host}`);
+    const waitTime = requestUrl.searchParams.get('wait');
+    const logOutput = createOutput('Incoimg request', req, currentDate, currentCount, requestId);
+
+    console.log(logOutput);
+
+    const respond = () => writeResponse(req, res, currentDate, currentDateString, currentCount, requestId);
+
+    if (waitTime === null) {
+        respond();
+    } else {
+        setTimeout(respond, waitTime);
+    }
+}
+
+http.createServer(handleRequest)
+.on('error', (error) => {
     console.error(error);
-}).on('listening', () => {
-    console.log('HTTP Server is listening now on port=%d with id=%s and node=%s', HTTP_PORT, UNIQUE_ID, process.version);
-}).listen(HTTP_PORT);
+})
+.on('listening', () => {
+    console.log(`HTTP Server is listening now on port=${httpPort} with id=${uniqueId} and node=${process.version}`);
+})
+.listen(httpPort);
 
